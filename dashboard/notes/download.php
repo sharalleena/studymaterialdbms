6f9f627aa91bd131f530607efb26425ad9e6c663
sharalleena <70796659+sharalleena@users.noreply.github.com>
2021-01-29 11:05:49 +0530

    Update download.php

--- dashboard/notes/download.php
+++ dashboard/notes/download.php
@@ -1,23 +1,11 @@
 <?php
-include "conn.php";
-if(isset($_GET['id'])){
-    $id=$_GET['id'];
-    $stat=$db->prepare("select * from notes1 where id=?");
-    $stat->bindParam(1,$id);
-    $stat->execute();
-    $data=$stat->fetch();
+$file=$_GET['file'].".pdf";
+header("content-disposition: attachment;filename=" .urlencode($file));
+$fb = fopen($file, "r");
+while(!feof($fb)){
+    echo fread($fb, 8192);
+    flush();
 
-    $file='upload/'.$data['filename'];
-    if(file_exists($file)){
-        header('Content-Description: '.$data['description']);
-        header('Content-Type: '.$data['type']);
-        header('Content-Disposition: '.$data['disposition'.': filename="'.basename($file).'"']);
-        header('Expires: '.$data['cache']);
-        header('Cache -Control: '.$data['description']);
-        header('Pragma: '.$data['pragma']);
-        header('Content-Length: ' .filesize($file));
-        readfile($file);
-        exit;
-
-    }
 }
+fclose($fb);
+?>
