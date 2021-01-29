93c886d3d15db9e8623df82948d27dff509fc928
sharalleena <70796659+sharalleena@users.noreply.github.com>
2021-01-29 10:29:39 +0530

    Create dash1.php

---
+++ dashboard/dash1.php
@@ -0,0 +1,28 @@
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="dashstyle.css">
+<meta name="viewport content="width=device-width,initial-scale=1.0">
+<title>Study Material for Engineering Students</title>
+</head>
+<body>
+      <div class="wrapper">
+            <div class="sidebar">
+                <h2>Dashboard</h2>
+                <ul>
+                        <li><a href="search.php">Browse books ads</a></li>
+                        <li><a href="notes1.php">Upload-notes</a></li>
+                        <li><a href="piv.html">advertise books</a></li>
+                        <li><a href="display1.php">Download-notes </a></li>
+                        
+        
+            </div>
+            <div class="main_content">
+                   <div class="header"><h1>Welcome! Have a nice day.</h1></div>
+                   <div class="info">
+                          <div></div>
+                   </div>
+            
+            </div>
+        </body>
+        </html>
